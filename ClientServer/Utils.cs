7007fa5d2bcdf6620a330ad4f8aa7a03232518b4
Matúš Moravčík <xmorav48@stud.fit.vut.cz>
2024-03-01 16:50:46 +0100

    feat: Add utility class for byte manipulation

---
+++ ClientServer/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServer;
+
+public class Utils {
+  public static byte[] AsBytes(byte type, short id, params string[] parameters) {
+    using MemoryStream stream = new();
+    using BinaryWriter writer = new(stream);
+
+    writer.Write(type);
+    writer.Write(id);
+
+    // Write values to the stream
+    foreach (var param in parameters) {
+      byte[] stringBytes = Encoding.UTF8.GetBytes(param);
+      writer.Write(stringBytes);
+      writer.Write((byte)0);
+    }
+
+    // Get the resulting byte array
+    return stream.ToArray();
+  }
+
+
+  // Find strings based on \0 separator in `data`
+  public static string[] FromBytes(byte[] data, int index = 0) {
+    string str = Encoding.UTF8.GetString(data, index, data.Length - index);
+    return str.Split("\0", StringSplitOptions.RemoveEmptyEntries);
+  }
+
+}
