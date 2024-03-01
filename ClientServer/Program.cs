862e3a6e475ec4b89135b4d187583dbfe68a80d3
Matúš Moravčík <xmorav48@stud.fit.vut.cz>
2024-03-01 10:33:29 +0100

    feat: Add command line parsing logic

---
+++ ClientServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using CommandLine;
+
+namespace ClientServer {
+
+  public class Program {
+    public class Options {
+      [Option('t', Required = true, HelpText = "Transport protocol used for connection")]
+      public required string Client { get; set; }
+
+      [Option('s', Required = true, HelpText = "Server IP or hostname")]
+      public required string Hostname { get; set; }
+
+      [Option('p', Default = 4567, Required = false, HelpText = "Server port")]
+      public int Port { get; set; }
+
+      [Option('d', Default = 250, Required = false, HelpText = "UDP confirmation timeout (ms)")]
+      public int Timeout { get; set; }
+
+      [Option('r', Default = 3, Required = false,
+        HelpText = "Maximum number of UDP retransmissions")]
+      public int Retransmissions { get; set; }
+    }
+
+    private static void Main(string[] args) {
+      Parser.Default.ParseArguments<Options>(args)
+        .WithParsed(o => {
+        });
+    }
+  }
+}
