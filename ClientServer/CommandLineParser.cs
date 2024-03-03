7fb4c399c105c8d286601b232e10970ae8e5fb8d
Matúš Moravčík <xmorav48@stud.fit.vut.cz>
2024-03-03 15:19:21 +0100

    refactor: Move command line parsing functionality into own file

---
+++ ClientServer/CommandLineParser.cs
@@ -0,0 +1,31 @@
+﻿using CommandLine;
+
+namespace ClientServer;
+
+public class CommandLineParser {
+  public class Options {
+    [Option('t', Required = true, HelpText = "Transport protocol used for connection")]
+    public required string Client { get; set; }
+
+    [Option('s', Required = true, HelpText = "Server IP or hostname")]
+    public required string Hostname { get; set; }
+
+    [Option('p', Default = 4567, Required = false, HelpText = "Server port")]
+    public int Port { get; set; }
+
+    [Option('d', Default = 250, Required = false, HelpText = "UDP confirmation timeout (ms)")]
+    public int Timeout { get; set; }
+
+    [Option('r', Default = 3, Required = false,
+      HelpText = "Maximum number of UDP retransmissions")]
+    public int Retransmissions { get; set; }
+  }
+
+  public static Options GetCommandLineArguments(string[] args) {
+    ParserResult<Options> result = Parser.Default.ParseArguments<Options>(args);
+
+    result.WithNotParsed((errors) => Environment.Exit(1));
+
+    return result.Value;
+  }
+}
--- ClientServer/Program.cs
+++ ClientServer/Program.cs
@@ -1,30 +1,19 @@
-﻿using CommandLine;
+﻿using ClientServer.Clients;
 
 namespace ClientServer {
-
   public class Program {
-    public class Options {
-      [Option('t', Required = true, HelpText = "Transport protocol used for connection")]
-      public required string Client { get; set; }
+    public static async Task Main(string[] args) {
+      CommandLineParser.Options options = CommandLineParser.GetCommandLineArguments(args);
 
-      [Option('s', Required = true, HelpText = "Server IP or hostname")]
-      public required string Hostname { get; set; }
+      BaseClient client = options.Client switch {
+        "tcp" => new TcpClient(options.Hostname, (ushort)options.Port),
+        "udp" => new UdpClient(options.Hostname, (ushort)options.Port, options.Timeout,
+          options.Retransmissions),
+        _ => throw new ArgumentException("Invalid client name"),
+      };
 
-      [Option('p', Default = 4567, Required = false, HelpText = "Server port")]
-      public int Port { get; set; }
-
-      [Option('d', Default = 250, Required = false, HelpText = "UDP confirmation timeout (ms)")]
-      public int Timeout { get; set; }
-
-      [Option('r', Default = 3, Required = false,
-        HelpText = "Maximum number of UDP retransmissions")]
-      public int Retransmissions { get; set; }
-    }
-
-    private static void Main(string[] args) {
-      Parser.Default.ParseArguments<Options>(args)
-        .WithParsed(o => {
-        });
+      ChatClient chat = new(client);
+      await chat.Communicate();
     }
   }
 }
