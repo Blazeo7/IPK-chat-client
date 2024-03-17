2fb98025abeb3a5fb437e96defcf940ea82b6bc0
Matúš Moravčík <xmorav48@vutbr.cz>
2024-03-17 19:11:15 +0100

    refactor: client is represented as `IClient` instead of `BaseClient`

--- ClientServer/Program.cs
+++ ClientServer/Program.cs
@@ -2,26 +2,26 @@
 
 namespace ClientServer;
 
-  public class Program {
-    public static async Task Main(string[] args) {
-      CommandLineParser.Options options = CommandLineParser.GetCommandLineArguments(args);
+public class Program {
+  public static async Task Main(string[] args) {
+    CommandLineParser.Options options = CommandLineParser.GetCommandLineArguments(args);
 
-      BaseClient client = options.Client switch {
-        "tcp" => new TcpClient(options.Hostname, (ushort)options.Port),
-        "udp" => new UdpClient(options.Hostname, (ushort)options.Port, options.Timeout,
-          options.Retransmissions),
-        _ => throw new ArgumentException("Invalid client name"),
-      };
+    IClient client = options.Client switch {
+      "tcp" => new TcpClient(options.Hostname, (ushort)options.Port),
+      "udp" => new UdpClient(options.Hostname, (ushort)options.Port, options.Timeout,
+        options.Retransmissions),
+      _ => throw new ArgumentException("Invalid client name"),
+    };
 
-      Logger.Init(options.Verbose);
+    Logger.Init(options.Verbose);
     Logger.Log("Starting program");
 
-      ChatClient chat = new(client);
-      try {
-        await chat.Communicate();
-      } catch (Exception e) {
+    ChatClient chat = new(client);
+    try {
+      await chat.Communicate();
+    } catch (Exception e) {
       Logger.Log($"Exception: {e.Message}");
-        Environment.Exit(1);
-      }
+      Environment.Exit(1);
     }
-  }
+  }
+}
