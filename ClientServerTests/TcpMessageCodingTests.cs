9367d7531c2854e82e9513948add0de4d6780384
Matúš Moravčík <xmorav48@vutbr.cz>
2024-03-17 19:53:54 +0100

    test: Use common test data to validate both encoding and decoding

--- ClientServerTests/TcpMessageCodingTests.cs
+++ ClientServerTests/TcpMessageCodingTests.cs
@@ -1,23 +1,29 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
+﻿using ClientServer.Enums;
 using ClientServer.Messages;
 
 namespace ClientServerTests;
 
 public class TcpMessageCodingTests {
-  public static IEnumerable<object[]> TestDataTcp() {
+  public static IEnumerable<object[]> CommonTcpTestData() {
   // @formatter:off
-    yield return [new AuthMessage("name", "pass", "alias"), "AUTH name AS alias USING pass\r\n"];
-    yield return [new JoinMessage("general", "alias"), "JOIN general AS alias\r\n"];
+    yield return [new AuthMessage("test", "secret", "Display_name"), "AUTH test AS Display_name USING secret\r\n"];
+    yield return [new JoinMessage("channel", "Display_name"), "JOIN channel AS Display_name\r\n"];
     yield return [new ByeMessage(), "BYE\r\n"];
-    yield return [new TextMessage(displayName: "alias", content: "test message"),"MSG FROM alias IS test message\r\n"];
-    yield return [new ErrorMessage(displayName: "alias", content: "test message"), "ERR FROM alias IS test message\r\n"];
+    yield return [new TextMessage(DisplayName: "Display_name", Content: "test message"),"MSG FROM Display_name IS test message\r\n"];
+    yield return [new ErrorMessage(DisplayName: "Display_name", Content: "test message"), "ERR FROM Display_name IS test message\r\n"];
+    yield return [new ReplyMessage(ReplyResult.Ok,  "Ok"), "REPLY OK IS Ok\r\n"];
+    yield return [new ReplyMessage(ReplyResult.Nok,  "Nok"), "REPLY NOK IS Nok\r\n"];
+    yield return [new InvalidMessage("INVALID MESSAGE"), "INVALID MESSAGE\r\n"];
     // @formatter:on
+  }
+
+  public static IEnumerable<object[]> FromTcp_TestData_() {
+    yield return [new InvalidMessage("INVALID MESSAGE"), "REPLY XXX IS Ok\r\n"];
   }
 
 
   [Theory]
-  [MemberData(nameof(TestDataTcp))]
+  [MemberData(nameof(CommonTcpTestData))]
   public void ToTcpFormat_ShouldReturnCorrectString(Message message, string expected) {
     // Arrange & Act
     string result = message.ToTcpFormat();
@@ -26,28 +32,15 @@ public class TcpMessageCodingTests {
     Assert.Equal(expected, result);
   }
 
-  [Fact]
-  public void Test() {
-    var arr = new byte[] { 82, 69, 80, 76, 89, 32, 79, 75, 32, 73, 83, 32, 79, 75, 13, 10, 0, 0 };
-    string byteArrayAsString = Encoding.ASCII.GetString(arr).TrimEnd('\0');
-    string regexPattern = @"^REPLY (OK|NOK) IS ([ -~]+)\r\n$";
-    var x = Regex.Match(byteArrayAsString, regexPattern);
-    Assert.True(x.Success);
-  }
-
   [Theory]
-  [InlineData("MSG FROM Alice IS Hello\r\n", typeof(TextMessage))]
-  [InlineData("REPLY OK IS Success\r\n", typeof(ReplyMessage))]
-  [InlineData("REPLY NOK IS Failure\r\n", typeof(ReplyMessage))]
-  [InlineData("ERR FROM Server IS Error Message\r\n", typeof(ErrorMessage))]
-  [InlineData("BYE\r\n", typeof(ByeMessage))]
-  [InlineData("InvalidMessageFormat\r\n",
-    typeof(ErrorMessage))] // Example of an invalid message format
-  public void FromTcpFormat_ShouldReturnCorrectMessageType(string tcpMessage, Type expectedType) {
+  [MemberData(nameof(CommonTcpTestData))]
+  [MemberData(nameof(FromTcp_TestData_))]
+  public void FromTcpFormat_ShouldReturnCorrectMessageType(Message expectedMessage,
+    string tcpMessage) {
     // Act
     Message result = Message.FromTcpFormat(tcpMessage);
 
     // Assert
-    Assert.IsType(expectedType, result);
+    Assert.Equal(expectedMessage, result);
   }
 }
